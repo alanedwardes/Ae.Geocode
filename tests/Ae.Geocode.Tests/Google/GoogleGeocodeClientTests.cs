b7cb864d26b41f9e573741fcccf18a508495ba30
Alan Edwardes <alan@alanedwardes.com>
2021-12-13 21:16:08 +0000

    Adds local test data.

--- tests/Ae.Geocode.Tests/Google/GoogleGeocodeClientTests.cs
+++ tests/Ae.Geocode.Tests/Google/GoogleGeocodeClientTests.cs
@@ -1,9 +1,7 @@
-﻿using Ae.Geocode.Google;
-using Ae.Geocode.Google.Entities;
-using System;
+﻿using Ae.Geocode.Google.Entities;
+using System.IO;
 using System.Linq;
-using System.Net.Http;
-using System.Threading;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -11,59 +9,52 @@ namespace Ae.Geocode.Tests.Google
 {
     public sealed class GoogleGeocodeClientTests
     {
-        private readonly IGoogleGeocodeClient _geocodeClient = new GoogleGeocodeClient(new HttpClient
-        {
-            BaseAddress = new Uri("https://maps.googleapis.com/")
-        });
-
-        private readonly string API_KEY = Environment.GetEnvironmentVariable("GOOGLE_MAPS_API_KEY");
-
         [Theory]
-        [InlineData(53.741697, -0.313853, "Victoria Park,Kingston upon Hull,Hull,England,Great Britain,United Kingdom")]
-        [InlineData(53.229116, -1.615255, "Chatsworth House,Chatsworth,Bakewell,Derbyshire Dales District,Derbyshire,England,Great Britain,United Kingdom")]
-        [InlineData(44.591133, 33.506989, "Leninsky District,Sevastopol,Crimean Peninsula")]
-        [InlineData(51.533317, 0.713909, "Southend-on-Sea,England,Great Britain,United Kingdom")]
-        [InlineData(56.413901, 159.294959, "Bystrinsky District,Kamchatka Krai,Russia")]
-        [InlineData(23.675719, 120.977318, "人和村,Xinyi Township,Nantou County,Taiwan,Taiwan Province")]
-        [InlineData(53.334107, -6.271100, "Portobello,Dublin 8,Dublin City,Dublin,County Dublin,Ireland")]
-        [InlineData(42.418829, 27.692754, "Tsentar,Sozopol,Burgas,Bulgaria")]
-        [InlineData(44.835961, -0.569279, "Hôtel de Ville - Quinconces,Canton de Bordeaux-1,Downtown,Bordeaux,Arrondissement de Bordeaux,Gironde,Nouvelle-Aquitaine,France")]
-        [InlineData(40.782864, -73.965355, "Central Park West Historic District,Manhattan,New York County,New York,United States")]
-        [InlineData(51.508873, -0.160518, "City of Westminster,London,Greater London,England,Great Britain,United Kingdom")]
-        [InlineData(54.173207, -2.167186, "Litton,Skipton,Craven District,North Yorkshire,England,Great Britain,United Kingdom")]
-        [InlineData(53.372927, -0.934697, "Lound,Retford,Bassetlaw District,Nottinghamshire,England,Great Britain,United Kingdom")]
-        [InlineData(53.300852, 0.1541388, "Withern with Stain,Alford,East Lindsey District,Lincolnshire,England,Great Britain,United Kingdom")]
-        [InlineData(42.356497, 23.383783, "Samokov Municipality,Sofia Province,Bulgaria")]
-        [InlineData(43.080720, -79.069865, "Niagara Falls,Niagara County,New York,United States")]
-        [InlineData(64.139029, -21.855955, "Sund,Háaleiti,Reykjavík,Capital Region,Iceland")]
-        [InlineData(64.600526, -21.063457, "Western Region,Iceland")]
-        [InlineData(55.066939, -4.478690, "Newton Stewart,Dumfries and Galloway,Scotland,Great Britain,United Kingdom")]
-        [InlineData(60.318686, -1.510724, "Shetland,Shetland Islands,Scotland,United Kingdom")]
-        [InlineData(26.714219, -41.772814, "Atlantic Ocean")]
-        [InlineData(-16.714647, -151.008915, "Maeva,Huahine,Leeward Islands,French Polynesia")]
-        [InlineData(-42.928412, 171.755361, "Selwyn District,Canterbury,New Zealand")]
-        [InlineData(-38.661171, 178.022943, "Gisborne,Gisborne District,New Zealand")]
-        [InlineData(-42.883679, 147.327609, "Hobart,City of Hobart,Tasmania,Australia")]
-        [InlineData(-23.317540, 135.727730, "Hart,Central Desert Shire,Northern Territory,Australia")]
-        [InlineData(75.455555, 139.598239, "Bulunsky District,Sakha Republic,Russia")]
-        [InlineData(-71.009811, -67.972639, "Antarctica")]
-        [InlineData(-56.510591, -68.728173, "Cabo de Hornos,Chile")]
-        [InlineData(-19.061144, -169.869073, "Alofi,Niue")]
-        [InlineData(52.029149, -3.209366, "Three Cocks,Gwernyfed,Brecon,Powys,Wales,Great Britain,United Kingdom")]
-        [InlineData(51.782667, -4.722841, "Templeton,Narberth,Pembrokeshire,Wales,Great Britain,United Kingdom")]
-        [InlineData(55.109070, -6.418931, "Stranocum,Ballymoney,Causeway Coast and Glens,Northern Ireland,Ireland,United Kingdom")]
-        [InlineData(52.617720, -7.637490, "Glengoole South,South Tipperary,County Tipperary,Ireland")]
-        [InlineData(81.408560, 91.242273, "Russia")]
-        [InlineData(79.117123, 14.239917, "Svalbard")]
-        [InlineData(12.164736, 15.031309, "N'Djamena,N'Djaména,Chad")]
-        [InlineData(0.334177, 32.582489, "Kitante,Kampala Central Division,Kampala,Kyadondo,Central Region,Uganda")]
-        [InlineData(30.054448, 31.239990, "Oraby,Al Azbakeya,Cairo,Cairo Governorate,Egypt")]
-        [InlineData(40.223220, 29.011015, "İstiklal,Bursa,Osmangazi,Turkey")]
-        [InlineData(28.640334, 77.281621, "Gyan Kunj,Guru Angad Nagar West,Laxmi Nagar,East Delhi,Delhi,India")]
-        public async Task Test(double latitude, double longitude, string expected)
+        [InlineData(1, "Victoria Park,Kingston upon Hull,Hull,England,United Kingdom")]
+        [InlineData(2, "Chatsworth House,Chatsworth,Bakewell,Derbyshire Dales District,Derbyshire,England,United Kingdom")]
+        [InlineData(3, "Leninsky District,Sevastopol,Crimean Peninsula")]
+        [InlineData(4, "Southend-on-Sea,England,United Kingdom")]
+        [InlineData(5, "Bystrinsky District,Kamchatka Krai,Russia")]
+        [InlineData(6, "人和村,Xinyi Township,Nantou County,Taiwan,Taiwan Province")]
+        [InlineData(7, "Portobello,Dublin 8,Dublin City,Dublin,County Dublin,Ireland")]
+        [InlineData(8, "Tsentar,Sozopol,Burgas,Bulgaria")]
+        [InlineData(9, "Hôtel de Ville - Quinconces,Canton de Bordeaux-1,Downtown,Bordeaux,Arrondissement de Bordeaux,Gironde,Nouvelle-Aquitaine,France")]
+        [InlineData(10, "Central Park West Historic District,Manhattan,New York County,New York,United States")]
+        [InlineData(11, "City of Westminster,London,Greater London,England,United Kingdom")]
+        [InlineData(12, "Litton,Skipton,Craven District,North Yorkshire,England,United Kingdom")]
+        [InlineData(13, "Lound,Retford,Bassetlaw District,Nottinghamshire,England,United Kingdom")]
+        [InlineData(14, "Withern with Stain,Alford,East Lindsey District,Lincolnshire,England,United Kingdom")]
+        [InlineData(15, "Samokov Municipality,Sofia Province,Bulgaria")]
+        [InlineData(16, "Niagara Falls,Niagara County,New York,United States")]
+        [InlineData(17, "Sund,Háaleiti,Reykjavík,Capital Region,Iceland")]
+        [InlineData(18, "Western Region,Iceland")]
+        [InlineData(19, "Newton Stewart,Dumfries and Galloway,Scotland,United Kingdom")]
+        [InlineData(20, "Shetland,Shetland Islands,Scotland,United Kingdom")]
+        [InlineData(21, "Atlantic Ocean")]
+        [InlineData(22, "Maeva,Huahine,Leeward Islands,French Polynesia")]
+        [InlineData(23, "Selwyn District,Canterbury,New Zealand")]
+        [InlineData(24, "Gisborne,Gisborne District,New Zealand")]
+        [InlineData(25, "Hobart,City of Hobart,Tasmania,Australia")]
+        [InlineData(26, "Hart,Central Desert Shire,Northern Territory,Australia")]
+        [InlineData(27, "Bulunsky District,Sakha Republic,Russia")]
+        [InlineData(28, "Antarctica")]
+        [InlineData(29, "Cabo de Hornos,Chile")]
+        [InlineData(30, "Alofi,Niue")]
+        [InlineData(31, "Three Cocks,Gwernyfed,Brecon,Powys,Wales,United Kingdom")]
+        [InlineData(32, "Templeton,Narberth,Pembrokeshire,Wales,United Kingdom")]
+        [InlineData(33, "Stranocum,Ballymoney,Causeway Coast and Glens,Northern Ireland,Ireland,United Kingdom")]
+        [InlineData(34, "Glengoole South,South Tipperary,County Tipperary,Ireland")]
+        [InlineData(35, "Russia")]
+        [InlineData(36, "Svalbard")]
+        [InlineData(37, "N'Djamena,N'Djaména,Chad")]
+        [InlineData(38, "Kitante,Kampala Central Division,Kampala,Kyadondo,Central Region,Uganda")]
+        [InlineData(39, "Oraby,Al Azbakeya,Cairo,Cairo Governorate,Egypt")]
+        [InlineData(40, "İstiklal,Bursa,Osmangazi,Turkey")]
+        [InlineData(41, "Gyan Kunj,Guru Angad Nagar West,Laxmi Nagar,East Delhi,Delhi,India")]
+        public async Task Test(int file, string expected)
         {
-            var location = (latitude, longitude);
-            var response = await _geocodeClient.ReverseGeoCode(new GeocodeRequest(API_KEY, location), CancellationToken.None);
+            using var stream = File.OpenRead("Google/Files/response" + file + ".json");
+            var response = await JsonSerializer.DeserializeAsync<GeocodeResponse>(stream, null);
             var components = string.Join(',', response.GetSimpleLocation().Select(x => x.LongName));
             Assert.Equal(expected, components);
         }
